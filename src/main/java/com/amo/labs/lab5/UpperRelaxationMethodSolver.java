ea0d75f42030ec80cfbd8b8b146e43ab7735bd91
Dementris <bdlap.shl@gmail.com>
2023-05-27 20:55:05 +0300

    docs

--- src/main/java/com/amo/labs/lab5/UpperRelaxationMethodSolver.java
+++ src/main/java/com/amo/labs/lab5/UpperRelaxationMethodSolver.java
@@ -1,8 +1,21 @@
 package com.amo.labs.lab5;
 
+/**
+ * The type Upper relaxation method solver.
+ */
 public class UpperRelaxationMethodSolver {
 
 
+    /**
+     * Method solver() which sole linear equation with upper relaxation method
+     *
+     * @param A             the matrix
+     * @param b             the constant numbers
+     * @param x             the x
+     * @param omega         the relaxation param
+     * @param maxIterations the max iterations
+     * @param tolerance     the tolerance
+     */
     public static void solver(double[][] A, double[] b, double[] x, double omega,
                               int maxIterations, double tolerance) {
         int n = A.length;
